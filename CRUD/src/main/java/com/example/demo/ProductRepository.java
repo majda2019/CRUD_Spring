6fe7cab0e3bdda1cc49b8c12eb70ef71bed1d080
Majda <elgarni.majdaa@gmail.com>
2019-11-30 20:36:59 +0100

    CRUD_Spring

---
+++ CRUD/src/main/java/com/example/demo/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
